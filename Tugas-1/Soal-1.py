802dd2a82b0f79c291c4c2e606e1d6804ec9ebe8
Georgius Kenny <kennygun2308@gmail.com>
2021-03-26 22:44:41 +0700

    Tugas 2 dan revisi Tugas 1

--- Tugas-1/Soal-1.py
+++ Tugas-1/Soal-1.py
@@ -1,8 +1,8 @@
 a = "halo semua selamat datang di program ini!!!"
 print(a)
 nama = input("silahlan tulis nama anda: ")
-umur = input("silahkan tulis umur anda: ")
-tinggi= input("silahkan tulis tinggi anda: ")
+umur = int(input("silahkan tulis umur anda: "))
+tinggi = float(input("silahkan tulis tinggi anda: "))
 b = "Nama saya {}, Umur saya {} tahun dan tinggi saya {} cm. ". format(nama, umur, tinggi)
 print("")
 print(b)
---
+++ Tugas-2/Soal-1.py
@@ -0,0 +1,34 @@
+print("Selamat datang!")
+      
+
+while True:
+    print("---Menu---")
+    print("1.  Daftar Kontak")
+    print("2.  Tambah Kontak")
+    print("3.  Keluar")
+    pilih = int(input("Pilih menu: "))
+    if  pilih == 1:   
+        print("Amal")
+        print("0834267588")
+        print(x)
+        print(y)
+    elif pilih == 2: 
+        x = str(input("Nama: "))
+        y = int(input("No. Telepon: "))
+        print("Kontak berhasil ditambahkan")
+
+
+    elif pilih == 3:
+        print("program selesai, sampai  jumpa")
+        break
+    else:
+        print("menu tidak tersedia, silahkan menginput No. yang benar")
+        print("------------------------------------------------------")
+        
+    
+
+
+
+
+
+
